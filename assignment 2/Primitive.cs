57a0216dd1939da8a4a52a84e8a77cc5b4ade80b
ilhan kalkan/ <i.d.kalkan@students.uu.nl>
2017-05-11 16:20:46 +0200

    waat

--- assignment 2/Primitive.cs
+++ assignment 2/Primitive.cs
@@ -3,10 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OpenTK;
 
 namespace template
 {
     class Primitive
     {
+        //for sphere
+        Vector3 pos;
+        float radius;
+
+        //for plane
+        Vector3 normal;
+        float distance;
+
+        //for current stuff, we will change this once we have materials
+        Vector4 color; //uses float values for r, g, b
     }
 }
