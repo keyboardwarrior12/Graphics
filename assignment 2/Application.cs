ee0f2d9655d8b892da44971ecc1205388ebc73c3
GERWIN\gerwindekruijf <g.j.dekruijf@students.uu.nl>
2017-05-28 17:18:19 +0200

    Readme, Application Render (MOET -> STAAT IN OPDRACHT), distance en intersections moet gefixt worden, rays doen het

--- assignment 2/Application.cs
+++ assignment 2/Application.cs
@@ -3,18 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using template;
+using OpenTK;
 
 namespace template
 {
     class Application
     {
-        /*Raytracer r = new Raytracer();
-        public Application()
+        public Raytracer raytracer;
+        public Application(Raytracer r)
         {
-            r.Render();
+            raytracer = r;
+            raytracer.Render();
         }
-        */ 
         
+        private void KeyUp(object sender, EventArgs ea)
+        {
+            Vector3 camera = raytracer.camera.pos;
+            Vector3 vectorUp = camera + new Vector3(1, 1, 1);
+            raytracer.camera.pos = vectorUp;
+        }
+
+        private void KeyDown(object sender, EventArgs ea)
+        {
+
+        }
         //Keyboard/Mouse input
     }
 }
